9dc3d5d06c9e8c62d37090acb5befb88903bca22
Nicole Robbins <nicole.robbins@att.net>
2023-02-12 20:20:28 -0600

    add exports

---
+++ src/components/Nooks.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nooks(){
+    return(
+        <div id="home">
+            <h1>Welcome to My Island!</h1>
+        </div>
+    )
+}
+
+export default Nooks;
---
+++ src/components/Villagers.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Villagers(){
+    return(
+        <div id="home">
+            <h1>Welcome to My Island!</h1>
+        </div>
+    )
+}
+
+export default Villagers;
