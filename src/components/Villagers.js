ea0d194686a0bd40e3ac9bc57f4a12920cdc5cad
Nicole Robbins <nicole.robbins@att.net>
2023-02-12 21:16:50 -0600

    build villagers page

--- src/components/Villagers.js
+++ src/components/Villagers.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function Villagers(){
+    useEffect(() => {
+    fetch("http://localhost:3000/villagers")
+        .then((res) => res.json())
+        .then((data) => createVillagerCard(data))
+  })
+    function createVillagerCard(){
+        return data.map((villager) => 
+            <div key={villager.name}>
+                {image}
+                <h2>{villager.name}</h2>
+                <ul>{villager.about.map((info) => (
+                <li key={info.type}>{info.birthday}{info.type}</li>
+                ))}</ul>
+            </div>
+        )
+    }
+
     return(
-        <div id="home">
-            <h1>Welcome to My Island!</h1>
+        <div className="Villagers">
+            <h1>Villagers on My Island</h1>
+                {createVillagerCard}
         </div>
     )
 }
