4eceba6e948cf67bc8128df09cb2f1ee3196fb7b
Nicole Robbins <nicole.robbins@att.net>
2023-02-12 23:36:33 -0600

    add form outline

--- src/components/Nooks.js
+++ src/components/Nooks.js
@@ -4,7 +4,7 @@ function Nooks({inventoryList}){
     
     const itemList = 
      inventoryList.map((item) => (
-        <div key={item.name}>
+        <div key={item.name} className="card">
             <h2>{item.name}</h2>
             <ul>
                 <li>{item.cost} bells</li>
@@ -17,6 +17,17 @@ function Nooks({inventoryList}){
             <h1>Welcome to Nooks Cranny!</h1>
             <h2>Browse our offerings to add to your Inventory.</h2>
             {itemList}
+            <form>
+                <label>
+                Buy Something!
+                    <input
+                        type="text"
+                        name="prompt"
+                        value={formData.prompt}
+                        onChange={handleChange}
+                    />
+                </label>
+            </form>
         </div>
     )
 }
