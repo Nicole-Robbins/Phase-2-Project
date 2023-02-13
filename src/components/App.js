e219c9a3b5aee06d0debca694d42345add76ce3c
Nicole Robbins <nicole.robbins@att.net>
2023-02-12 20:18:46 -0600

    set up App

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import '../App.css';
+import NavBar from "./NavBar";
 import Home from "./Home";
+import Villagers from "./Villagers";
+import Nooks from "./Nooks";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <Home />
-      </header>
+            <NavBar />
+            <Switch>
+                <Route path="/home">
+                    <Home />
+                </Route>
+                <Route path="/villagers">
+                    <Villagers />
+                </Route>
+                <Route path="/nooks">
+                    <Nooks />
+                </Route>
+            </Switch>
     </div>
   );
 }
