1b3980067659b189ccaa85b7a27dfb31a3703418
Nicole Robbins <nicole.robbins@att.net>
2023-02-12 20:33:31 -0600

    add nav links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 function NavBar(){
     return(
-        <div id="home">
-            <h1>Welcome to My Island!</h1>
-        </div>
+        <nav>
+            <NavLink exact to="/home">Home</NavLink>
+            <NavLink exact to="/villagers">Villagers</NavLink>
+            <NavLink exact to="nooks">Nooks</NavLink>
+        </nav>
     )
 }
 
